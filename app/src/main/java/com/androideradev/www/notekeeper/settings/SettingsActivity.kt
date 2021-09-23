403360394cd30a8930c3947fe95021d5764ab382
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-09-23 11:35:37 +0200

    Use nav component to handle navigation between setting framgents

--- app/src/main/java/com/androideradev/www/notekeeper/settings/SettingsActivity.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/settings/SettingsActivity.kt
@@ -2,11 +2,39 @@ package com.androideradev.www.notekeeper.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.androideradev.www.notekeeper.R
+import com.androideradev.www.notekeeper.databinding.ActivitySettingsBinding
+
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
