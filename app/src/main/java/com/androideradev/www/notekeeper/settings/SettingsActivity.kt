ef0c399736e02c8c2e63ab4b9c607873ea6dfcd1
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-09-23 09:44:12 +0200

    Add Settings Activity files

---
+++ app/src/main/java/com/androideradev/www/notekeeper/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.androideradev.www.notekeeper.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androideradev.www.notekeeper.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
