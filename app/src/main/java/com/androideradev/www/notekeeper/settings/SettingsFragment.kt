7a33434f9771ab1c71e1ad2b10817094bdce1755
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-09-23 12:38:50 +0200

    Create preference settings resource file and inflate it from preference fragment

--- app/src/main/java/com/androideradev/www/notekeeper/settings/SettingsFragment.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/settings/SettingsFragment.kt
@@ -1,35 +1,19 @@
 package com.androideradev.www.notekeeper.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
 import com.androideradev.www.notekeeper.R
 
-class SettingsFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        view.findViewById<Button>(R.id.settings_fragment_button).setOnClickListener {
-            val navHostFragment =
-                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-            val navController = navHostFragment.navController
-
-            navController.navigate(R.id.action_settingsFragment_to_accountSettingsFragment)
-
-        }
-
-        return view
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
 
 
 }
+
+/* val navHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+
+            navController.navigate(R.id.action_settingsFragment_to_accountSettingsFragment)*/
