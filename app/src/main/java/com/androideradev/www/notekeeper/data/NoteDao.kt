dbb83f7c0f2963a0693528ffaf236dcc5063c3d1
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-09-30 09:19:52 +0200

    Implement notes query from database

--- app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
@@ -1,12 +1,17 @@
 package com.androideradev.www.notekeeper.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.androideradev.www.notekeeper.NoteInfo
 
 @Dao
 interface NoteDao {
 
     @Insert
-    fun addNote(note: NoteInfo)
+    fun insertNote(note: NoteInfo)
+
+    @Query("SELECT * FROM notes")
+    fun getAllNotes() : LiveData<List<NoteInfo>>
 }
