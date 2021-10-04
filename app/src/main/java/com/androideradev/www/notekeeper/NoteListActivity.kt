58b6dd5fce87b9e9a465f02554b8dab42210a306
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-10-04 14:22:25 +0200

    Observe the notes in database and pass the updated notes to the notes adapter
    Comment any other code in the NoteListAcitivty and only focus on query and display notes functionality

--- app/src/main/java/com/androideradev/www/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/NoteListActivity.kt
@@ -22,11 +22,11 @@ import com.androideradev.www.notekeeper.adapters.NoteRecyclerAdapter
 import com.androideradev.www.notekeeper.adapters.RecentViewedNoteRecyclerAdapter
 import java.util.*
 
-class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+class NoteListActivity : AppCompatActivity(), //NavigationView.OnNavigationItemSelectedListener,
     NoteRecyclerAdapter.OnNoteItemClickListener {
 
     private lateinit var binding: ActivityNoteListBinding
-    private val tag = NoteActivity::class.java.simpleName
+    private val tag = NoteListActivity::class.java.simpleName
 
     private val linearLayoutManager by lazy { LinearLayoutManager(this) }
 
@@ -35,15 +35,17 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
     //Default use of property without lazy clause will cause runtime error because we cant access Context
     //before on create method and  any property get instantiate when the activity
     // class instance created and before the call to onCreate method
-    private val noteRecyclerAdapter by lazy { NoteRecyclerAdapter(this, DataManager.notes, this) }
-
-
-    private val recentlyViewedNoteRecyclerAdapter by lazy {
-        RecentViewedNoteRecyclerAdapter(this, viewModel.recentlyViewedNotes)
-
+    private val noteRecyclerAdapter by lazy {
+        NoteRecyclerAdapter(this, viewModel.notesFromDatabase.value ?: listOf<NoteInfo>(), this)
     }
 
-    private val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+
+    /* private val recentlyViewedNoteRecyclerAdapter by lazy {
+         RecentViewedNoteRecyclerAdapter(this, viewModel.recentlyViewedNotes)
+
+     }*/
+
+    /*private val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
         ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT
     ) {
         override fun onMove(
@@ -53,60 +55,82 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         ): Boolean {
             // Position of the list item want to drag
             val startPosition = viewHolder.adapterPosition
+
+
             // Position of the list item we want to swap with the dragged item
             val toPosition = targetViewHolder.adapterPosition
 
             // First swap the items in the list
-            Collections.swap(DataManager.notes, startPosition, toPosition)
+            // Collections.swap(DataManager.notes, startPosition, toPosition)
+
+            // val data = viewModel.notesFromDatabase.value?.toMutableList()!!
+            //   Log.i(tag, "Data Before Swap $data")
+            *//*    Collections.swap(
+                    data,
+                    startPosition,
+                    toPosition
+                )*//*
+
+            // Log.i(tag, "Data After Swap In OnSwap ${data}")
+
             // Notify the adapter of the items change positions
             recyclerView.adapter?.notifyItemMoved(startPosition, toPosition)
+
+            // viewModel.updateNotes(data)
+
             return true
         }
 
+        override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+            super.clearView(recyclerView, viewHolder)
+            // Log.i(tag, "Data After Swap ${viewModel.notesFromDatabase.value}")
+
+        }
+
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            // Position of Note swiped item
-            val position = viewHolder.adapterPosition
-            // Note object to delete
-            val deletedNote = DataManager.notes[position]
+             // Position of Note swiped item
+             val position = viewHolder.adapterPosition
+             // Note object to delete
+             val deletedNote = DataManager.notes[position]
 
-            // Delete Item
-            DataManager.notes.removeAt(position)
-            Log.i(tag, "After remove the First item :${DataManager.notes}")
-            noteRecyclerAdapter.notifyItemRemoved(position)
-            noteRecyclerAdapter.notifyItemRangeChanged(position, DataManager.notes.size)
+             // Delete Item
+             DataManager.notes.removeAt(position)
+             Log.i(tag, "After remove the First item :${DataManager.notes}")
+             noteRecyclerAdapter.notifyItemRemoved(position)
+             noteRecyclerAdapter.notifyItemRangeChanged(position, DataManager.notes.size)
 
-            // Position of deleted note inside the recent viewed notes if exist if not return -1
-            val recentDeletedNotePosition = viewModel.recentlyViewedNotes.indexOf(deletedNote)
-            if (recentDeletedNotePosition != -1) {
-                // Delete the Item from recent Viewed also if exist
-                viewModel.recentlyViewedNotes.removeAt(recentDeletedNotePosition)
-                // Notify recentViewAdapter of the deleted Item Position
-                recentlyViewedNoteRecyclerAdapter.notifyItemRemoved(recentDeletedNotePosition)
-            }
+             // Position of deleted note inside the recent viewed notes if exist if not return -1
+             val recentDeletedNotePosition = viewModel.recentlyViewedNotes.indexOf(deletedNote)
+             if (recentDeletedNotePosition != -1) {
+                 // Delete the Item from recent Viewed also if exist
+                 viewModel.recentlyViewedNotes.removeAt(recentDeletedNotePosition)
+                 // Notify recentViewAdapter of the deleted Item Position
+                 recentlyViewedNoteRecyclerAdapter.notifyItemRemoved(recentDeletedNotePosition)
+             }
 
-            //Show snack bar to undo the delete
-            Snackbar.make(
-                binding.appBarNoteList.contentNoteList.notesRecyclerView,
-                "Deleted",
-                Snackbar.LENGTH_LONG
-            ).setAction("UNDO") {
-                // Back the deleted note to the list if user undo the delete
-                DataManager.notes.add(position, deletedNote)
-                Log.i(tag, "After Add Back the First item :${DataManager.notes}")
-                noteRecyclerAdapter.notifyItemInserted(position)
-                noteRecyclerAdapter.notifyItemRangeChanged(position, DataManager.notes.size)
+             //Show snack bar to undo the delete
+             Snackbar.make(
+                 binding.appBarNoteList.contentNoteList.notesRecyclerView,
+                 "Deleted",
+                 Snackbar.LENGTH_LONG
+             ).setAction("UNDO") {
+                 // Back the deleted note to the list if user undo the delete
+                 DataManager.notes.add(position, deletedNote)
+                 Log.i(tag, "After Add Back the First item :${DataManager.notes}")
+                 noteRecyclerAdapter.notifyItemInserted(position)
+                 noteRecyclerAdapter.notifyItemRangeChanged(position, DataManager.notes.size)
 
 
-                if (recentDeletedNotePosition != -1) {
-                    //Back the deleted note to recent notes if exist previous
-                    viewModel.recentlyViewedNotes.add(recentDeletedNotePosition, deletedNote)
-                    // Notify recentViewAdapter of the inserted item Position
-                    recentlyViewedNoteRecyclerAdapter.notifyItemInserted(recentDeletedNotePosition)
-                }
-            }.show()
+                 if (recentDeletedNotePosition != -1) {
+                     //Back the deleted note to recent notes if exist previous
+                     viewModel.recentlyViewedNotes.add(recentDeletedNotePosition, deletedNote)
+                     // Notify recentViewAdapter of the inserted item Position
+                     recentlyViewedNoteRecyclerAdapter.notifyItemInserted(recentDeletedNotePosition)
+                 }
+             }.show()
         }
     })
-
+*/
     private lateinit var viewModel: NoteListActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -124,6 +148,7 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         // When the activity destroy and re-created due to configuration change
         // the activity receive the same instance of the viewModel that was created when the activity initially created
         viewModel = ViewModelProvider(this)[NoteListActivityViewModel::class.java]
+
 
         // We depend viewModel to restore nav display selection and recent viewed notes due to configuration change
         // We only want to use savedInstanceState when the viewModel destroyed which happened when
@@ -149,7 +174,7 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        binding.navView.setNavigationItemSelectedListener(this)
+        //   binding.navView.setNavigationItemSelectedListener(this)
 
 
     }
@@ -172,29 +197,30 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         binding.appBarNoteList.contentNoteList.notesRecyclerView.adapter =
             noteRecyclerAdapter
         viewModel.notesFromDatabase.observe(this, { notesFromDatabase ->
+            Log.i(tag, "Note From Database After Swap $notesFromDatabase")
             noteRecyclerAdapter.setNotesFromDatabase(notesFromDatabase)
         })
         // Attach the the touch helper to recycler view only if the user select nav notes display
-        itemTouchHelper.attachToRecyclerView(binding.appBarNoteList.contentNoteList.notesRecyclerView)
+       // itemTouchHelper.attachToRecyclerView(binding.appBarNoteList.contentNoteList.notesRecyclerView)
     }
 
-    private fun displayCourses() {
-        binding.appBarNoteList.contentNoteList.notesRecyclerView.layoutManager =
-            GridLayoutManager(
-                this,
-                resources.getInteger(R.integer.courses_span_count),
-                RecyclerView.VERTICAL,
-                false
-            )
+    /* private fun displayCourses() {
+         binding.appBarNoteList.contentNoteList.notesRecyclerView.layoutManager =
+             GridLayoutManager(
+                 this,
+                 resources.getInteger(R.integer.courses_span_count),
+                 RecyclerView.VERTICAL,
+                 false
+             )
 
-        binding.appBarNoteList.contentNoteList.notesRecyclerView.adapter =
-            CourseRecyclerAdapter(this, DataManager.courses.values.toList())
-        //Not apply drag or swap to delete to courses nav display selection since we only operate on
-        //notes list in the DataManager
+         binding.appBarNoteList.contentNoteList.notesRecyclerView.adapter =
+             CourseRecyclerAdapter(this, DataManager.courses.values.toList())
+         //Not apply drag or swap to delete to courses nav display selection since we only operate on
+         //notes list in the DataManager
         itemTouchHelper.attachToRecyclerView(null)
-    }
+     }*/
 
-    private fun displayRecentlyViewedNotes() {
+    /*private fun displayRecentlyViewedNotes() {
         binding.appBarNoteList.contentNoteList.notesRecyclerView.layoutManager =
             linearLayoutManager
         binding.appBarNoteList.contentNoteList.notesRecyclerView.adapter =
@@ -203,12 +229,8 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         //notes list in the DataManager
         itemTouchHelper.attachToRecyclerView(null)
     }
+*/
 
-    override fun onResume() {
-        super.onResume()
-
-        binding.appBarNoteList.contentNoteList.notesRecyclerView.adapter?.notifyDataSetChanged()
-    }
 
     override fun onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -218,7 +240,7 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+/*    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_notes,
@@ -252,7 +274,7 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
 
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
-    }
+    }*/
 
     private fun handleDisplaySelection(itemId: Int) {
         when (itemId) {
@@ -263,11 +285,11 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
 
             }
             R.id.nav_courses -> {
-                displayCourses()
+                //    displayCourses()
 
             }
             R.id.nav_recently_viewed -> {
-                displayRecentlyViewedNotes()
+                //   displayRecentlyViewedNotes()
 
 
             }
@@ -298,11 +320,13 @@ class NoteListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
     }
 
     override fun onNoteItemDelete(note: NoteInfo) {
-        val isRemoved = viewModel.recentlyViewedNotes.remove(note)
+        /* val isRemoved = viewModel.recentlyViewedNotes.remove(note)
 
-        if (isRemoved) {
-            Log.i(tag, "Recently Note Removed: $isRemoved")
-        }
+         if (isRemoved) {
+             Log.i(tag, "Recently Note Removed: $isRemoved")
+         }*/
+
+        viewModel.deleteNote(note)
 
     }
 
