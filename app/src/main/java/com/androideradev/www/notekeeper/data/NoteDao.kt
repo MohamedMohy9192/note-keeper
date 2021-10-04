20acbaa48f7097aad6ac79a411b05f186fedf839
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-10-04 14:18:56 +0200

    Update Dao interface

--- app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
@@ -1,9 +1,7 @@
 package com.androideradev.www.notekeeper.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import com.androideradev.www.notekeeper.NoteInfo
 
 @Dao
@@ -15,5 +13,23 @@ interface NoteDao {
     /*Room generates all the necessary code to update the LiveData object when a database is updated.
     The generated code runs the query asynchronously on a background thread when needed.*/
     @Query("SELECT * FROM notes")
-    fun getAllNotes() : LiveData<List<NoteInfo>>
+    fun getAllNotes(): LiveData<List<NoteInfo>>
+
+
+    @Update
+    fun updateNote(vararg note: NoteInfo)
+
+    /*    @Query("SELECT * FROM notes WHERE id = :id")
+        fun getNote(id: Int) : LiveData<NoteInfo>*/
+
+
+    @Update
+    fun updateNotes(notes: List<NoteInfo>)
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNote(id: Int): NoteInfo
+
+    @Delete
+    fun deleteNote(note :NoteInfo)
+
 }
