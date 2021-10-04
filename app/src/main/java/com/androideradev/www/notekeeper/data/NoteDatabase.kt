426903fcb715ec8abf43967b625972d660bd1bf0
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-10-04 16:57:08 +0200

    Write migration for adding new column in note table

--- app/src/main/java/com/androideradev/www/notekeeper/data/NoteDatabase.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/data/NoteDatabase.kt
@@ -4,14 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.androideradev.www.notekeeper.NoteInfo
 
 @Database(entities = [NoteInfo::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
 
-    abstract fun noteDao() : NoteDao
+    abstract fun noteDao(): NoteDao
 
     companion object {
+        // First we need to increase the version in the Database Annotation
+        // We write migration to preserve the exist data from lose when change the schema of database
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE  notes ADD COLUMN description TEXT DEFAULT 'Add Description' NOT NULL")
+            }
+
+        }
+
         private var noteDatabaseInstance: NoteDatabase? = null
 
         fun getDatabase(context: Context): NoteDatabase? {
@@ -22,7 +33,8 @@ abstract class NoteDatabase : RoomDatabase() {
                         noteDatabaseInstance = Room.databaseBuilder<NoteDatabase>(
                             context.applicationContext,
                             NoteDatabase::class.java, "note_database"
-                        ).build()
+                        )//.addMigrations(MIGRATION_1_2)
+                            .build()
                     }
                 }
             }
