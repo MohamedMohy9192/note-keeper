3b498ba7a3137f56fe2c1f1ff005c11abe1d4ae2
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-10-13 00:10:52 +0200

    Update NoteDao.kt

--- app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/data/NoteDao.kt
@@ -7,9 +7,13 @@ import com.androideradev.www.notekeeper.NoteInfo
 @Dao
 interface NoteDao {
 
-    @Insert
-    fun insertNote(note: NoteInfo)
+    @Insert()
+    fun insertNote(note: NoteInfo): Long
 
+
+    @Insert
+    fun insertNotes(notes: List<NoteInfo>): List<Long>
+    
     /*Room generates all the necessary code to update the LiveData object when a database is updated.
     The generated code runs the query asynchronously on a background thread when needed.*/
     @Query("SELECT * FROM notes")
