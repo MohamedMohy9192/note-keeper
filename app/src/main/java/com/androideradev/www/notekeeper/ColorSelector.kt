a928bf65dba1882969140f1509d63260d1ba947b
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-10-24 05:47:28 +0200

    Fix color selector view not changing color when arrows clicked

--- app/src/main/java/com/androideradev/www/notekeeper/ColorSelector.kt
+++ app/src/main/java/com/androideradev/www/notekeeper/ColorSelector.kt
@@ -5,14 +5,19 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 
 class ColorSelector @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0,
 ) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-    private val colors = listOf(Color.RED, Color.BLUE, Color.GRAY)
+    private val colors = listOf(Color.RED, Color.BLUE, Color.GREEN)
     private var selectedColorIndex = 0
+
+    private var leftArrowImageView: ImageView
+    private var rightArrowImageView: ImageView
+    private var colorView: View
 
     init {
         orientation = HORIZONTAL
@@ -21,32 +26,42 @@ class ColorSelector @JvmOverloads constructor(
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = layoutInflater.inflate(R.layout.color_selector, this)
 
-        view.findViewById<View>(R.id.color_select_view)
-            .setBackgroundColor(colors[selectedColorIndex])
+        colorView = view.findViewById(R.id.color_select_view)
+        colorView.setBackgroundColor(colors[selectedColorIndex])
 
-        view.setOnClickListener {
+        leftArrowImageView = view.findViewById(R.id.color_select_left_arrow)
+
+        leftArrowImageView.setOnClickListener {
             selectPreviousColor()
         }
 
-        view.setOnClickListener {
+        rightArrowImageView = view.findViewById(R.id.color_select_right_arrow)
+
+        rightArrowImageView.setOnClickListener {
             selectNextColor()
         }
     }
 
     private fun selectNextColor() {
-        if (selectedColorIndex == colors.lastIndex){
+        if (selectedColorIndex == colors.lastIndex) {
             selectedColorIndex = 0
-        }
 
-        selectedColorIndex++
-        colors[selectedColorIndex]
+        }else{
+            selectedColorIndex++
+        }
+        colorView.setBackgroundColor(colors[selectedColorIndex])
+
     }
 
     private fun selectPreviousColor() {
-       if (selectedColorIndex == 0){
-           selectedColorIndex = colors.lastIndex
-       }
-        selectedColorIndex--
-        colors[selectedColorIndex]
+        if (selectedColorIndex == 0) {
+            selectedColorIndex = colors.lastIndex
+
+        }else{
+            selectedColorIndex--
+        }
+
+        colorView.setBackgroundColor(colors[selectedColorIndex])
+
     }
 }
